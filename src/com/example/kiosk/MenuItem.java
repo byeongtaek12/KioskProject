ac059e492f3a2ca9f8e95b6c23fbb9f016dd886b
obt <byeongtaek12@naver.com>
2025-03-07 12:50:18 +0900

    feat: MenuItem 클래스의 속성과 생성자 추가
    ``

--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,4 +1,13 @@
 package com.example.kiosk;
 
 public class MenuItem {
+    String name;
+    double price;
+    String explanation;
+
+    MenuItem(String name, double price, String explanation) {
+        this.name = name;
+        this.price = price;
+        this.explanation = explanation;
+    }
 }
