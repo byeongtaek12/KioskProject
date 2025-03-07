66407258f75ec0151c3a78a9b1fa20704fc238f1
obt <byeongtaek12@naver.com>
2025-03-07 16:04:09 +0900

    style: 안 쓰는 import문 제거
    ``

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -1,8 +1,5 @@
 package com.example.kiosk;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Kiosk kiosk = new Kiosk();
