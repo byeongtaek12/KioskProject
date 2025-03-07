39443281cb4c616924385167af08d43553b144ee
obt <byeongtaek12@naver.com>
2025-03-07 16:36:44 +0900

    Go to lv3

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -2,6 +2,8 @@ package com.example.kiosk;
 
 public class Main {
     public static void main(String[] args) {
+        
+
         Kiosk kiosk = new Kiosk();
         kiosk.start();
     }
--- src/com/example/kiosk/Menu.java
+++ src/com/example/kiosk/Menu.java
@@ -1,4 +1,20 @@
 package com.example.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
+    String category;
+    List<MenuItem> menuItems = new ArrayList<>();
+
+    public Menu(String category) {
+        this.category = category;
+    }
+
+    public void showMenu() {
+
+    }
+
+
+
 }
