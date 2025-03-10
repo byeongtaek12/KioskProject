759ab3e0b0eadf7c0d57a71aac316ecfeba512a6
obt <byeongtaek12@naver.com>
2025-03-10 10:20:29 +0900

    refactor: 멤버 변수 캡슐화
    ``

--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,13 +1,23 @@
 package com.example.kiosk;
 
 public class MenuItem {
-    String name;
-    double price;
-    String explanation;
+    private String name;
+    private double price;
+    private String explanation;
 
     MenuItem(String name, double price, String explanation) {
         this.name = name;
         this.price = price;
         this.explanation = explanation;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getprice() {
+        return price;
+    }
+    public String getexplanation() {
+        return explanation;
     }
 }
