348f18ee2bd8adc0480d1b695212df3a304f4ffe
obt <byeongtaek12@naver.com>
2025-03-10 11:05:59 +0900

    style: 메뉴 내용 변경 및 메서드 이름 변경
    ``

--- src/com/example/kiosk/Menu.java
+++ src/com/example/kiosk/Menu.java
@@ -21,16 +21,16 @@ public class Menu {
                 menuItems.add(new MenuItem("Hamburger   ",5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
             }
             case "Drinks" -> {
-                menuItems.add(new MenuItem("Water ",6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-                menuItems.add(new MenuItem("SmokeShack  ",8.9, " 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-                menuItems.add(new MenuItem("Cheeseburger",6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-                menuItems.add(new MenuItem("Hamburger   ",5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+                menuItems.add(new MenuItem("Water           ",1.0, "리얼 제로 칼로리 청정수"));
+                menuItems.add(new MenuItem("Coke & zero Coke",2.0, "펩시 주문 안 받아요"));
+                menuItems.add(new MenuItem("MilkShake       ",2.5, "신선한 우유가 들어간 부드러운 밀크셰이크"));
+                menuItems.add(new MenuItem("Sprite          ",2.0, "목이 너무 따갑지만 청량감 원탑"));
             }
             case "Desserts" -> {
-                menuItems.add(new MenuItem("CheeseCake ",6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-                menuItems.add(new MenuItem("SmokeShack  ",8.9, " 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-                menuItems.add(new MenuItem("Cheeseburger",6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-                menuItems.add(new MenuItem("Hamburger   ",5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+                menuItems.add(new MenuItem("CheeseCake     ",4.0, "내가 좋아해서 파는 메뉴"));
+                menuItems.add(new MenuItem("CheeseStick    ",2.0, "치즈 2m 늘어남"));
+                menuItems.add(new MenuItem("Ice Cream      ",2.0, "달달하이 쳐 쥑이네"));
+                menuItems.add(new MenuItem("Chicken Nuggets",2.0, "단백질 부족하신 분 더 사드세요"));
             }
             default -> System.out.println("카테고리에 있는 것만 선택해주세요");
         }
@@ -43,7 +43,7 @@ public class Menu {
     public void showChildMenuCategory() {
         int i = menuItems.size()-1;
         for (MenuItem menuItem : menuItems) {
-            System.out.println(menuItems.size()-i +". " + menuItem.getName() + "  | W " + menuItem.getprice() + " | " + menuItem.getexplanation());
+            System.out.println(menuItems.size()-i +". " + menuItem.getName() + "  | W " + menuItem.getPrice() + " | " + menuItem.getExplanation());
             i--;
         }
     }
