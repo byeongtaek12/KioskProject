f90ceccc7efcd3ea2eb1ce699373a91700973c36
obt <byeongtaek12@naver.com>
2025-03-10 11:07:38 +0900

    refactor: 예외 처리 및 switch문 -> if문으로 교체
    반복문 교체 이유 : 중복 로직 제거

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -1,5 +1,6 @@
 package com.example.kiosk;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Kiosk {
@@ -11,7 +12,14 @@ public class Kiosk {
         while (flag) {
             menu.showParentMenuCategory();
             System.out.print("메뉴를 선택해주세요 : ");
-            int chooseParentMenu = sc.nextInt();
+            int chooseParentMenu;
+            try {
+                chooseParentMenu = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("올바르지 않은 입력입니다");
+                sc.nextLine();
+                continue;
+            }
             switch (chooseParentMenu) {
                 case 0 -> {
                     System.out.println("프로그램을 종료합니다");
@@ -29,30 +37,39 @@ public class Kiosk {
                     menu = new Menu("Desserts");
                     System.out.println("선택한 메인메뉴 :  3. "+ menu.getCategory());
                 }
-                default -> System.out.println("메뉴에 있는 숫자만 써주세요");
+                default -> {
+                    System.out.println("메뉴에 있는 숫자만 써주세요");
+                    continue;
+                }
             }
+
             if (chooseParentMenu==0) {
                 break;
             }
-            System.out.println();
-            System.out.println("[ "+ menu.getCategory()+ "MENU"+ " ]");
-            menu.showChildMenuCategory();
-            System.out.println("0. 되돌아가기      | 되돌아가기");
-            System.out.print("메뉴를 선택해주세요 : ");
-            int chooseChildMenu = sc.nextInt();
-            if (chooseChildMenu == 0) {
-                System.out.println("되돌아갑니다");
-            }else {
-                System.out.println("선택한 " + menu.getCategory() + "메뉴 :  1. " + menu.getMenuItems().get(0).getName() + "  | W " + menu.getMenuItems().get(0).getprice() + " | " + menu.getMenuItems().get(0).getexplanation());
-            }
+            while (true) {
+                System.out.println();
+                System.out.println("[ " + menu.getCategory() + "MENU" + " ]");
+                menu.showChildMenuCategory();
+                System.out.println("0. 되돌아가기      | 되돌아가기");
+                System.out.print("메뉴를 선택해주세요 : ");
+                int chooseChildMenu;
+                try {
+                    chooseChildMenu = sc.nextInt();
+                }catch (InputMismatchException e) {
+                    System.out.println("올바르지 않은 입력입니다");
+                    sc.nextLine();
+                    continue;
+                }
 
-            switch (chooseChildMenu) {
-                case 0 -> System.out.println("되돌아갑니다");
-                case 1 -> System.out.println("선택한 " + menu.getCategory() + "메뉴 :  1. " + menu.getMenuItems().get(0).getName() + "  | W " + menu.getMenuItems().get(0).getprice() + " | " + menu.getMenuItems().get(0).getexplanation());
-                case 2 -> System.out.println("선택한 "+ menu.getCategory() + "메뉴 :  2. " + menu.getMenuItems().get(1).getName() + "  | W " + menu.getMenuItems().get(1).getprice() + " | " + menu.getMenuItems().get(1).getexplanation());
-                case 3 -> System.out.println("선택한 " + menu.getCategory() + "메뉴 :  3. " + menu.getMenuItems().get(2).getName() + "  | W " + menu.getMenuItems().get(2).getprice() + " | " + menu.getMenuItems().get(2).getexplanation());
-                case 4 -> System.out.println("선택한 " + menu.getCategory() + "메뉴 :  4. " + menu.getMenuItems().get(3).getName() + "  | W " + menu.getMenuItems().get(3).getprice() + " | " + menu.getMenuItems().get(3).getexplanation());
-                default -> System.out.println("메뉴에 있는 숫자만 써주세요");
+                if (chooseChildMenu == 0) {
+                    System.out.println("되돌아갑니다");
+                } else if (chooseChildMenu > 0 && chooseChildMenu < 5) {
+                    System.out.println("선택한 " + menu.getCategory() + "메뉴 :  " + chooseChildMenu + ". " + menu.getMenuItems().get(chooseChildMenu - 1).getName() + "  | W " + menu.getMenuItems().get(chooseChildMenu - 1).getPrice() + " | " + menu.getMenuItems().get(chooseChildMenu - 1).getExplanation());
+                } else {
+                    System.out.println("메뉴에 있는 숫자만 써주세요");
+                    continue;
+                }
+                break;
             }
         }
     }
