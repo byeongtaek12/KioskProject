3b0b3d3764fa9422b3ebe2623f3fe3854f7eb5e1
obt <byeongtaek12@naver.com>
2025-03-10 17:48:53 +0900

    feat: 쇼핑카트 클래스 추가
    ``

---
+++ src/com/example/kiosk/ShoppingCart.java
@@ -0,0 +1,34 @@
+package com.example.kiosk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+     List<String> shoppingCart = new ArrayList<>();
+     String name;
+     int count;
+     int price;
+
+    public ShoppingCart() {
+    }
+
+    public ShoppingCart (String name, int count, int price) {
+         this.name = name;
+         this.count = count;
+         this.price = price;
+     }
+
+
+     public void addShoppingCart(String menu) {
+         this.shoppingCart.add(menu);
+     }
+
+     public List<String> getShoppingCart() {
+         return this.shoppingCart;
+     }
+
+    public void removeShoppingCart() {
+        this.shoppingCart.clear();
+    }
+
+}
