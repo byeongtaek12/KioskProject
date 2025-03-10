7728602363902fe121594d1a1c0bb2a425ef7012
obt <byeongtaek12@naver.com>
2025-03-10 17:59:03 +0900

    feat: 장바구니 및 구매하기 기능 추가
    장바구니 생성 및 관리 기능, 장바구니 출력 및 금액 계산, 장바구니 담기 기능, 주문 기능 추가

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Kiosk {
     private Menu menu = new Menu();
+    private ShoppingCart shoppingCart = new ShoppingCart();
     private final List<Menu> listMenus = new ArrayList<>();
     private final Scanner sc = new Scanner(System.in);
     private boolean flag = true;
@@ -61,16 +62,67 @@ public class Kiosk {
                     sc.nextLine();
                     continue;
                 }
-
                 if (chooseChildMenu == 0) {
                     System.out.println("되돌아갑니다");
                 } else if (chooseChildMenu > 0 && chooseChildMenu < 5) {
                     System.out.println("선택한 " + menu.getCategory() + "메뉴 :  " + chooseChildMenu + ". " + menu.getMenuItems().get(chooseChildMenu - 1).getName() + "  | W " + menu.getMenuItems().get(chooseChildMenu - 1).getPrice() + " | " + menu.getMenuItems().get(chooseChildMenu - 1).getExplanation());
+                    System.out.println();
+                    System.out.println();
+                    System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).getName() + "  | W " + menu.getMenuItems().get(chooseChildMenu - 1).getPrice() + " | " + menu.getMenuItems().get(chooseChildMenu - 1).getExplanation());
+                    System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                    System.out.println("1. 확인        2. 취소");
+                    System.out.print("번호를 선택해주세요 : ");
+                    int chooseShoppingCartMenu = sc.nextInt();
+                    if (chooseShoppingCartMenu==1) {
+                        shoppingCart.addShoppingCart(menu.getMenuItems().get(chooseChildMenu - 1).getName() + "  | W " + menu.getMenuItems().get(chooseChildMenu - 1).getPrice() + " | " + menu.getMenuItems().get(chooseChildMenu - 1).getExplanation());
+                        System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).getName()+"이 장바구니에 추가되었습니다");
+                    }else {
+                        System.out.println("취소되었습니다");
+                    }
                 } else {
                     System.out.println("메뉴에 있는 숫자만 써주세요");
                     continue;
                 }
                 break;
+            }
+            if (!shoppingCart.getShoppingCart().isEmpty()) {
+                System.out.println("[ ORDER MENU" + " ]");
+                System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.");
+                System.out.println("5. Cancel       | 진행중인 주문을 취소합니다.");
+                System.out.print("번호를 선택해주세요 : ");
+                int chooseOrderMenu = sc.nextInt();
+                System.out.println();
+                if (chooseOrderMenu==4) {
+                    System.out.println("[ Orders" + " ]");
+                    double sum = 0;
+                    for (int i = 0; i<shoppingCart.getShoppingCart().size(); i++) {
+                        String [] findPrice = shoppingCart.getShoppingCart().get(i).split("\\|");
+                        String [] findPrice1 = findPrice[1].split(" ");
+                        double findPrice2 = Double.parseDouble(findPrice1[2]);
+                        System.out.println(shoppingCart.getShoppingCart().get(i));
+                        sum += findPrice2;
+                    }
+                    System.out.println();
+                    System.out.println("[ Total" + " ]");
+                    System.out.println("W "+sum);
+                    System.out.println();
+                    System.out.println("1. 주문      2. 메뉴판");
+                    System.out.print("번호를 선택해주세요 : ");
+                    int chooseOrderAndMenu = sc.nextInt();
+                    if (chooseOrderAndMenu == 1) {
+                        System.out.println("주문이 완료되었습니다. 금액은 W "+sum +" 입니다");
+                        shoppingCart.removeShoppingCart();
+                        break;
+                    }else if (chooseOrderAndMenu==2) {
+                        System.out.println("메뉴판으로 되돌아갑니다");
+                    }else {
+                        System.out.println("1,2번 중에 골라주세요");
+                    }
+                }else if (chooseOrderMenu==5) {
+                    System.out.println("진행중인 주문을 취소했습니다");
+                }else{
+                    System.out.println("1,2번 중에 골라주세요");
+                }
             }
         }
     }
