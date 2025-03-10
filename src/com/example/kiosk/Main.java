38571dae9531474c8547f5d4b8cc7ef2bd6434b2
obt <byeongtaek12@naver.com>
2025-03-10 10:21:10 +0900

    style: 안 쓰는 부분 정리
    ``

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -2,8 +2,6 @@ package com.example.kiosk;
 
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu("Burgers");
-
         Kiosk kiosk = new Kiosk();
         kiosk.start();
     }
