64561e4a509b428e49fabf042a11146e4f206045
obt <byeongtaek12@naver.com>
2025-03-11 19:21:13 +0900

    refactor: while문 제거
    입출력 메서드를 빼주니까 while문으로 구역을 나눠줄 필요가 없어져서 제거

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -36,76 +36,63 @@ public class Kiosk {
                 menu = listMenus.get(chooseParentMenu - 1);
                 System.out.println("선택한 메인메뉴 :  " + chooseParentMenu + ". " + menu.getCategory());
             }
-            while (true) {
+            System.out.println();
+            System.out.println("[ " + menu.getCategory() + "MENU" + " ]");
+            menu.showMenuCategory();
+            System.out.println("0. 되돌아가기      | 되돌아가기");
+            System.out.println();
+            int chooseChildMenu = io("메뉴를 선택해주세요 : ",0,4);
+            if (chooseChildMenu == 0) {
+                System.out.println("되돌아갑니다");
+                continue;
+            } else {
+                System.out.println("선택한 " + menu.getCategory() + "메뉴 :  " + chooseChildMenu + ". " + menu.getMenuItems().get(chooseChildMenu - 1).toString());
                 System.out.println();
-                System.out.println("[ " + menu.getCategory() + "MENU" + " ]");
-                menu.showMenuCategory();
-                System.out.println("0. 되돌아가기      | 되돌아가기");
                 System.out.println();
-                int chooseChildMenu = io("메뉴를 선택해주세요 : ",0,4);
-                if (chooseChildMenu == 0) {
-                    System.out.println("되돌아갑니다");
+                System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).toString());
+                System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
+                System.out.println("1. 확인        2. 취소");
+                int chooseShoppingCartMenu = io("번호를 선택해주세요 : ", 1, 2);
+                if (chooseShoppingCartMenu == 1) {
+                    shoppingCart.addShoppingCart(menu.getMenuItems().get(chooseChildMenu - 1).toString());
+                    System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).getName() + "이 장바구니에 추가되었습니다");
                 } else {
-                    while (true) {
-                        System.out.println("선택한 " + menu.getCategory() + "메뉴 :  " + chooseChildMenu + ". " + menu.getMenuItems().get(chooseChildMenu - 1).toString());
-                        System.out.println();
-                        System.out.println();
-                        System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).toString());
-                        System.out.println("위 메뉴를 장바구니에 추가하시겠습니까?");
-                        System.out.println("1. 확인        2. 취소");
-                        int chooseShoppingCartMenu = io("번호를 선택해주세요 : ",1,2);
-                        if (chooseShoppingCartMenu==1) {
-                            shoppingCart.addShoppingCart(menu.getMenuItems().get(chooseChildMenu - 1).toString());
-                            System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).getName() + "이 장바구니에 추가되었습니다");
-                        }else{
-                            System.out.println("취소되었습니다");
-                        }
-                        break;
-                    }
+                    System.out.println("취소되었습니다");
                 }
-                break;
             }
             if (!shoppingCart.getShoppingCart().isEmpty()) {
                 int chooseOrderMenu;
-                while(true) {
-                    System.out.println();
-                    System.out.println("[ ORDER MENU" + " ]");
-                    System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.\n 5. Cancel       | 진행중인 주문을 취소합니다.");
-                    chooseOrderMenu = io("메뉴를 선택해주세요 : ",4,5);
-                    if (chooseOrderMenu==4) {
-                        while (true) {
-                            System.out.println("[ Orders" + " ]");
-                            double sum = 0;
-                            for (int i = 0; i<shoppingCart.getShoppingCart().size(); i++) {
-                                String [] findPrice = shoppingCart.getShoppingCart().get(i).split("\\|");
-                                String [] findPrice1 = findPrice[1].split(" ");
-                                double findPrice2 = Double.parseDouble(findPrice1[2]);
-                                System.out.println(shoppingCart.getShoppingCart().get(i));
-                                sum += findPrice2;
-                            }
-                            System.out.println();
-                            System.out.println("[ Total" + " ]");
-                            System.out.println("W "+sum);
-                            System.out.println();
-                            System.out.println("1. 주문      2. 메뉴판");
-                            chooseOrderAndMenu = io("번호를 선택해주세요 : ",1,2);
-                            if (chooseOrderAndMenu==1) {
-                                System.out.println("주문이 완료되었습니다. 금액은 W " + sum + " 입니다");
-                                shoppingCart.removeShoppingCart();
-                            }else {
-                                System.out.println("메뉴판으로 되돌아갑니다");
-                            }
-                            break;
-                        }
-                    }else {
-                        System.out.println("진행중인 주문을 취소했습니다");
+                System.out.println();
+                System.out.println("[ ORDER MENU" + " ]");
+                System.out.println("4. Orders       | 장바구니를 확인 후 주문합니다.\n 5. Cancel       | 진행중인 주문을 취소합니다.");
+                chooseOrderMenu = io("메뉴를 선택해주세요 : ", 4, 5);
+                if (chooseOrderMenu == 4) {
+                    System.out.println("[ Orders" + " ]");
+                    double sum = 0;
+                    for (int i = 0; i < shoppingCart.getShoppingCart().size(); i++) {
+                        String[] findPrice = shoppingCart.getShoppingCart().get(i).split("\\|");
+                        String[] findPrice1 = findPrice[1].split(" ");
+                        double findPrice2 = Double.parseDouble(findPrice1[2]);
+                        System.out.println(shoppingCart.getShoppingCart().get(i));
+                        sum += findPrice2;
                     }
-                    break;
+                    System.out.println();
+                    System.out.println("[ Total" + " ]");
+                    System.out.println("W " + sum);
+                    System.out.println();
+                    System.out.println("1. 주문      2. 메뉴판");
+                    chooseOrderAndMenu = io("번호를 선택해주세요 : ", 1, 2);
+                    if (chooseOrderAndMenu == 1) {
+                        System.out.println("주문이 완료되었습니다. 금액은 W " + sum + " 입니다");
+                        shoppingCart.removeShoppingCart();
+                        break;
+                    } else {
+                        System.out.println("메뉴판으로 되돌아갑니다");
+                    }
+                } else {
+                    System.out.println("진행중인 주문을 취소했습니다");
                 }
             }
-           if (chooseOrderAndMenu==1) {
-               break;
-           }
         }
     }
     // 입출력 메서드
