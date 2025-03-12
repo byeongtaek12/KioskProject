dd4765f1dad545189c0a3449ed56c6fae615ee5d
obt <byeongtaek12@naver.com>
2025-03-12 20:03:07 +0900

    feat: 새로운 클래스를 추가
    튜터님의 말씀: ShoppingCart의 책임을 분산시켜줄 수 있는 클래스를 만들어주면 좋을 것 같다고 하심.

---
+++ src/com/example/kiosk/ShoppingCartService.java
@@ -0,0 +1,25 @@
+package com.example.kiosk;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCartService {
+    private final List<ShoppingCart> shoppingCartlist;
+
+    public ShoppingCartService(List<ShoppingCart> shoppingCartlist) {
+        this.shoppingCartlist = shoppingCartlist;
+    }
+
+    // TODO 카트 안에 있는 아이템의 가격 합산
+
+
+    // TODO 카트에 담겨있는 아이템 출력
+
+
+    // TODO 카트 비우기
+
+
+    // TODO 카트에 아이템 추가하기
+
+
+}
