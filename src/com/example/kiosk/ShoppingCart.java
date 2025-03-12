fe08e4d22448b4002d5dc67c8fe5593564bdc0c9
obt <byeongtaek12@naver.com>
2025-03-12 11:31:39 +0900

    feat: 인스턴스 변수 캡슐화 및 생성자와 getter 추가
    ``

--- src/com/example/kiosk/ShoppingCart.java
+++ src/com/example/kiosk/ShoppingCart.java
@@ -4,12 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-     List<String> shoppingCart = new ArrayList<>();
-     String name;
-     int count;
-     int price;
+     private final List<String> shoppingCart = new ArrayList<>();
+     private String name;
+     private int count;
+     private double price;
 
     public ShoppingCart() {
+    }
+
+    public ShoppingCart(String name, int count, double price) {
+        this.name=name;
+        this.count=count;
+        this.price=price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public double getPrice() {
+        return this.price;
     }
 
      public void addShoppingCart(String menu) {
@@ -23,5 +41,4 @@ public class ShoppingCart {
     public void removeShoppingCart() {
         this.shoppingCart.clear();
     }
-
 }
