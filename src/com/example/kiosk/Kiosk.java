ec3253e6c1f1c7efc10a2b76a809dc48abb5939a
obt <byeongtaek12@naver.com>
2025-03-13 16:13:27 +0900

    feat: shoppingCartAddCancel 메서드에 for문 생성
    같은 메뉴가 담길 시 뒤에 추가되는 메뉴를 삭제하고 수량을 증가시키는 for문 생성

--- src/com/example/kiosk/Kiosk.java
+++ src/com/example/kiosk/Kiosk.java
@@ -95,8 +95,17 @@ public class Kiosk {
                     menu.getMenuItems().get(chooseChildMenu - 1).getPrice()
             );
             shoppingCart.addShoppingCartItems(shoppingCartItemTemp);
-            System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).getName() + "이 장바구니에 추가되었습니다");
 
+            // 장바구니에 같은 메뉴가 들어왔을 때 사용
+            System.out.println(menu.getMenuItems().get(chooseChildMenu - 1).getName() + "이 장바구니에 추가되었습니다");
+            for (int i = 0; i<shoppingCart.getShoppingCartItems().size()-1; i++) {
+                for (int j = i+1; j<shoppingCart.getShoppingCartItems().size(); j++) {
+                    if (shoppingCart.getShoppingCartItems().get(i).getName().equals(shoppingCart.getShoppingCartItems().get(j).getName())) {
+                        shoppingCart.removeShoppingCartItems(j);
+                        shoppingCart.getShoppingCartItems().get(i).setCount();
+                    }
+                }
+            }
         } else {
             System.out.println("취소되었습니다");
         }
