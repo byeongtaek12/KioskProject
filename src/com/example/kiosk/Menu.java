f62812da915d560946175c0c4f1d2111e2b7abc6
obt <byeongtaek12@naver.com>
2025-03-13 10:10:25 +0900

    refactor: 멤버 변수들 초기화를 생성자를 통해 하게끔 수정
    ``

--- src/com/example/kiosk/Menu.java
+++ src/com/example/kiosk/Menu.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class Menu {
     private final String category;
-    private final List<MenuItem> menuItemlist = new ArrayList<>();
+    private final List<MenuItem> menuItemlist;
 
     public Menu(String category) {
         this.category=category;
+        menuItemlist=new ArrayList<>();
         switch (this.category) {
             case "Burgers" -> {
                 menuItemlist.add(new MenuItem("ShackBurger ",6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
