13da8f6de52294d0481a7976c2e22c1e78c2b0ac
obt <byeongtaek12@naver.com>
2025-03-14 09:41:03 +0900

    feat: Discount클래스) 이름이 type인 변수 생성
    유형별 한글 이름도 받아서 출력해주고 싶어서 생성

--- src/com/example/kiosk/Discount.java
+++ src/com/example/kiosk/Discount.java
@@ -1,16 +1,21 @@
 package com.example.kiosk;
 
 public enum Discount {
-    PERSONOFNATIONALMERIT("10%"), ARMY("5%"), STUDENT("3%"), GENERALPERSON("0%");
+    PERSONOFNATIONALMERIT("10%","국가유공자"), ARMY("5%","군인"), STUDENT("3%","학생"), GENERALPERSON("0%","일반인");
 
     private String discount;
+    private String type;
 
-    Discount (String discount) {
+    Discount (String discount, String type) {
         this.discount=discount;
+        this.type=type;
     }
 
     public String getDiscount() {
         return this.discount;
+    }
 
+    public String getType() {
+        return this.type;
     }
 }
