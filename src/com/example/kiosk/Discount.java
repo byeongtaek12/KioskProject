a1ac5591107784cac10f797adc584b73b257538f
obt <byeongtaek12@naver.com>
2025-03-14 09:48:40 +0900

    refactor: Discount클래스) 변하지 않는 값 final 처리
    ``

--- src/com/example/kiosk/Discount.java
+++ src/com/example/kiosk/Discount.java
@@ -3,8 +3,8 @@ package com.example.kiosk;
 public enum Discount {
     PERSONOFNATIONALMERIT("10%","국가유공자"), ARMY("5%","군인"), STUDENT("3%","학생"), GENERALPERSON("0%","일반인");
 
-    private String discount;
-    private String type;
+    private final String discount;
+    private final String type;
 
     Discount (String discount, String type) {
         this.discount=discount;
